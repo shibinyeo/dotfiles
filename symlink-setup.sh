5d912cd02a2e51b626efe90e30823303c9a36a04
shibinyeo <yeoshibin97@gmail.com>
2023-06-18 18:49:02 +0800

    added .gitconfig, symlink-setup.sh

---
+++ symlink-setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# To be run as the non-root user
+ln -sv ${HOME}/.dotfiles/autorandr ${HOME}/.config/autorandr
+ln -sv ${HOME}/.dotfiles/awesome ${HOME}/.config/awesome
+ln -sv ${HOME}/.dotfiles/.zshrc ${HOME}/.zshrc
+ln -sv ${HOME}/.dotfiles/.gitconfig ${HOME}/.gitconfig
