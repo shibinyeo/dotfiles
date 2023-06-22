a8cce9049f680f0603d02fc4ea838cff6a86195b
shibinyeo <yeoshibin97@gmail.com>
2023-06-22 23:47:37 +0800

    edited apps-install.sh

--- apps-install.sh
+++ apps-install.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # Official Repositories
+# Neglected (pipewire)
 sudo pacman -S caffeine-ng tigervnc flameshot neovim obsidian virt-manager autorandr bash-completion base-devel telegram-desktop obs-studio libreoffice-still \
-awesome-git wezterm rofi acpi acpid acpi_call upower lxappearance-gtk3 \
+wezterm rofi acpi acpid acpi_call upower lxappearance-gtk3 \
 jq inotify-tools polkit-gnome xdotool xclip gpick ffmpeg blueman redshift \
-pipewire pipewire-alsa pipewire-pulse alsa-utils brightnessctl feh maim --needed
+pipewire-alsa pipewire-pulse alsa-utils brightnessctl feh maim --needed
 
-# Download paru
+# Download paru (use "rust")
 cd ${HOME}/Downloads
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 
 # AUR
-paru -Sy microsoft-edge-beta-bin --needed
+paru -S microsoft-edge-beta-bin awesome-git --needed
 
 # Install yoru
 git clone --depth 1 --recurse-submodules https://github.com/rxyhn/yoru.git
