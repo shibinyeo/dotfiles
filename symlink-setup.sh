0d39c099fe60b3c6df251a46ec9b9c10830f3ff8
shibinyeo <yeoshibin97@gmail.com>
2023-06-23 13:27:17 +0800

    added onedrive.service and edited symlink-setup.sh to include it

--- symlink-setup.sh
+++ symlink-setup.sh
@@ -2,8 +2,14 @@
 
 # To be run as the non-root user
 rm -rf ${HOME}/.config/autorandr
-#rm -rf ${HOME}/.config/awesome
 ln -sfv ${HOME}/.dotfiles/autorandr ${HOME}/.config/autorandr
+
+#rm -rf ${HOME}/.config/awesome
 #ln -sfv ${HOME}/.dotfiles/awesome ${HOME}/.config/awesome
+
 ln -sfv ${HOME}/.dotfiles/.zshrc ${HOME}/.zshrc
+sudo ln -sfv ${HOME}/.dotfiles/.zshrc /root/.zshrc
+
 ln -sfv ${HOME}/.dotfiles/.gitconfig ${HOME}/.gitconfig
+
+sudo ln -sfv ${HOME}/.dotfiles/onedrive.service /etc/systemd/system/onedrive.service
