f4850d13a38a968a92b49db419da2c6b10641b7e
shibinyeo <yeoshibin97@gmail.com>
2024-03-16 13:55:30 +0800

    feat: Changed bash prompt and added pre-checks to symlink-setup.sh

--- .bashrc
+++ .bashrc
@@ -11,9 +11,6 @@ alias grep='grep --color=auto'
 # Enable unlimited history
 HISTSIZE= HISTFILESIZE= 
 
-# add new line and then make prompt red
-export PS1="\n\e[0;31m[\u@\h \W]\$ \e[m"
-
 # Aliases
 alias   l='eza -lhar -s modified --icons=auto' # long list all
 alias  ls='eza -1r -s modified --icons=auto' # short list
@@ -40,3 +37,7 @@ source /usr/share/fzf/key-bindings.bash
 export FZF_ALT_C_COMMAND="find -L . -mindepth 1 \( -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \) -prune  -o -type d -print 2> /dev/null | cut -b3-"
 export FZF_ALT_C_OPTS="--preview 'tree -C {}'"
 
+# Change Prompt
+tri='\uE0B0'
+PS1_t="\[\e[48;2;251;227;191m\]\[\e[30m\]\[\e[1m\] \u \[\e[0m\]\[\e[38;2;251;227;191m\]\[\e[44m\]$tri\[\e[30m\] \[\e[1m\]\w \[\e[0m\]\[\e[34m\]$tri\[\e[0m\] "
+PS1=$(echo -en "$PS1_t")
--- symlink-setup.sh
+++ symlink-setup.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
-# To be run as the non-root user
 
+# Make sure script runs as non-root
+if [ "$EUID" -eq 0 ]; then
+    echo "This script must NOT be run as root"
+    exit 1
+fi
+
+# Confirm every command before running
+BLUE='\033[0;34m'
+RESET='\033[0m'
+function _trap_DEBUG() {
+    echo -ne "${BLUE}\nCOMMAND:${RESET} $BASH_COMMAND \n${BLUE}(Enter)Run/(Q)uit: ${RESET}";
+    read choice
+    echo
+    case "$choice" in
+        q|Q) exit ;;
+        *) ;;
+    esac
+}
+
+trap "_trap_DEBUG" DEBUG
+
+# Setup Symlinks for Shell Configs
 ln -sfv ${HOME}/.dotfiles/.zshrc ${HOME}/.zshrc
 sudo ln -sfv ${HOME}/.dotfiles/.bash_profile /root/.bash_profile
 sudo ln -sfv ${HOME}/.dotfiles/.bashrc /root/.bashrc
 
+# Setup Symlinks for Git Config
 ln -sfv ${HOME}/.dotfiles/.gitconfig ${HOME}/.gitconfig
 sudo ln -sfv ${HOME}/.dotfiles/.gitconfig /root/.gitconfig
